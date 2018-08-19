27c0f4bf51f09e2c5330c7b5bd267cf98918c2b8
Christian Flach <cmfcmf.flach@gmail.com>
2018-08-19 16:15:59 +0200

    Improve upload instructions and error handling

--- src/upload.ts
+++ src/upload.ts
@@ -1,6 +1,9 @@
-import {JSZipObject} from "jszip";
 import * as JSZip from "jszip";
+import {JSZipObject} from "jszip";
 import * as log from "loglevel";
+import * as React from "react";
+import {ChangeEvent} from "react";
+import * as ReactDOM from "react-dom";
 import FileSystem from "./filesystem";
 import BSHParser from "./parsers/BSH/bsh-parser";
 import CODParser from "./parsers/COD/cod-parser";
@@ -11,120 +14,91 @@ import Stream from "./parsers/stream";
 import WAVParser from "./parsers/WAV/wav-parser";
 import BitmapFontGenerator from "./parsers/ZEI/bitmap-font-generator";
 import parseTranslations from "./translation/translation-parser";
+import UploadInfo from "./uploadInfo";
 import {findRootInZip} from "./util/util";
 
 const escapeStringRegexp = require("escape-string-regexp");
 
+export interface UploadLogger {
+    readonly info: (msg: string) => void;
+    readonly warn: (msg: string) => void;
+    readonly error: (msg: string) => void;
+}
+
 export default class UploadHandler {
-    constructor(private readonly fs: FileSystem) { }
+    private logger: UploadLogger;
+
+    constructor(private readonly fs: FileSystem) {
+        this.logger = log;
+    }
 
     public async init() {
         console.table(await this.fs.ls("/"));
-        // Doesn't work in Firefox
-        // console.log(await this.fs.df());
     }
 
     public render(game: HTMLElement) {
-        const uploadBtn = this.createUploadButton();
-        const saveOrMissionUploadButton = this.createSaveOrMissionUploadButton();
-        const resetBtn = this.createResetButton();
+        const notes = document.createElement("div");
+        ReactDOM.render(React.createElement(
+            UploadInfo, {
+                onUploaded: this.uploadAndParse.bind(this),
+                onReset: this.reset.bind(this),
+                onSaveOrMissionUploaded: async (evt: ChangeEvent<HTMLInputElement>) =>  {
+                    const files = evt.target.files;
+                    if (files === null || files.length === 0) {
+                        return;
+                    }
+                    await Promise.all(Array.from(files).map((file) => this.uploadSaveOrMission(file)));
+                    alert("Upload finished. The page will now refresh.");
+                    window.location.reload(true);
+                },
+                setLogger: this.setLogger.bind(this),
+            }), game);
 
-        game.appendChild(uploadBtn);
-        game.appendChild(saveOrMissionUploadButton);
-        game.appendChild(resetBtn);
+        game.appendChild(notes);
     }
 
     public async isUploaded() {
         return await this.fs.exists("/fields.dat");
     }
 
-    private createUploadButton() {
-        const uploadBtn = document.createElement("input");
-        uploadBtn.type = "file";
-        uploadBtn.multiple = false;
-        uploadBtn.accept = ".zip";
-        uploadBtn.onchange = async () => {
-            const files = uploadBtn.files;
-            if (files === null || files.length !== 1) {
-                alert("Please select the zipped Anno1602 folder to upload.");
-                return;
-            }
-            const file = files[0];
-            if (!file.name.endsWith(".zip")) {
-                alert("You need to upload a .zip file!");
-                return;
-            }
-            uploadBtn.disabled = true;
-            await this.uploadAndParse(file);
-
-            alert("Upload finished. The page will now refresh.");
-            window.location.reload(true);
-        };
-        return uploadBtn;
+    public setLogger(logger: UploadLogger) {
+        this.logger = logger;
     }
 
-    private createSaveOrMissionUploadButton() {
-        const uploadBtn = document.createElement("input");
-        uploadBtn.type = "file";
-        uploadBtn.multiple = true;
-        uploadBtn.accept = ".szm,.szs,.gam";
-        uploadBtn.onchange = async () => {
-            const files = uploadBtn.files;
-            if (files === null || files.length === 0) {
-                return;
-            }
-            uploadBtn.disabled = true;
-            await Promise.all(Array.from(files).map((file) => this.uploadSaveOrMission(file)));
-            alert("Upload finished. The page will now refresh.");
-            window.location.reload(true);
-        };
-        return uploadBtn;
-    }
-
-    private createResetButton() {
-        const resetBtn = document.createElement("button");
-        resetBtn.innerText = "Reset All Files";
-        resetBtn.onclick = async () => {
-            resetBtn.disabled = true;
-            await this.fs.rmRoot();
-            alert("All files deleted. The page will now refresh.");
-            window.location.reload(true);
-        };
-        return resetBtn;
+    private async reset() {
+        await this.fs.rmRoot();
     }
 
     private async uploadAndParse(file: File): Promise<boolean> {
-        await this.fs.write("/original.zip", file);
-        const zipFileEntry = await this.fs.open("/original.zip");
-
         let zip;
         try {
+            await this.fs.write("/original.zip", file);
+            const zipFileEntry = await this.fs.open("/original.zip");
             zip = await JSZip.loadAsync(zipFileEntry);
         } catch (e) {
-            this.error("Could not open the uploaded ZIP file.", e);
+            this.logger.error(`Could not upload the ZIP file: ${e.message}`);
             return false;
         }
 
-        let annoRoot;
         try {
-            annoRoot = findRootInZip(zip);
+            const annoRoot = findRootInZip(zip);
+            await this.fs.rmRoot();
+
+            await this.copyIslands(annoRoot);
+            await this.copySaves(annoRoot);
+            const customMissionPath = await this.parseTranslations(annoRoot);
+            await this.copyMissions(annoRoot, customMissionPath);
+            await this.decryptCODs(annoRoot);
+            await this.parseDATs();
+            await this.parseGADs(annoRoot);
+            await this.parseBSHs(annoRoot);
+            await this.parseZEIs(annoRoot);
+            await this.parseMusic(annoRoot);
+            await this.parseVideos(annoRoot);
         } catch (e) {
-            this.error("", e);
+            this.logger.error(e.message);
             return false;
         }
-        await this.fs.rmRoot();
-
-        await this.copyIslands(annoRoot);
-        await this.copySaves(annoRoot);
-        const customMissionPath = await this.parseTranslations(annoRoot);
-        await this.copyMissions(annoRoot, customMissionPath);
-        await this.decryptCODs(annoRoot);
-        await this.parseDATs();
-        await this.parseGADs(annoRoot);
-        await this.parseBSHs(annoRoot);
-        await this.parseZEIs(annoRoot);
-        await this.parseMusic(annoRoot);
-        await this.parseVideos(annoRoot);
 
         return true;
     }
@@ -137,10 +111,6 @@ export default class UploadHandler {
         }
     }
 
-    private error(msg: string, error: Error) {
-        alert(msg + "\n" + error.message);
-    }
-
     private async parseDATs() {
         return Promise.all([
             ["/fields.dat", "/fields.json"],
@@ -148,13 +118,13 @@ export default class UploadHandler {
         ].map(async (r) => {
             const inName = r[0];
             const outName = r[1];
-            log.info(`Started parsing "${inName}".`);
+            this.logger.info(`Started parsing "${inName}".`);
 
             const parser = new DATParser();
             const data = parser.parse(await this.fs.openAndGetContentAsText(inName));
             await this.fs.write(outName, JSON.stringify(data));
 
-            log.info(`Finished parsing "${inName}".`);
+            this.logger.info(`Finished parsing "${inName}".`);
         }));
     }
 
@@ -222,7 +192,7 @@ export default class UploadHandler {
             }
             const inName = `GADDATA/${r[0]}`;
             const outName = `/screens/${r[1]}.json`;
-            log.info(`Started parsing "${inName}".`);
+            this.logger.info(`Started parsing "${inName}".`);
 
             const parser = new DATParser();
 
@@ -230,7 +200,7 @@ export default class UploadHandler {
             const data = parser.parse(await gadFile.async("text"));
             await this.fs.write(outName, JSON.stringify(data));
 
-            log.info(`Finished parsing "${inName}".`);
+            this.logger.info(`Finished parsing "${inName}".`);
         }
     }
 
@@ -244,13 +214,13 @@ export default class UploadHandler {
             const inName = r[0];
             const outName = r[1];
 
-            log.info(`Started parsing "${inName}".`);
+            this.logger.info(`Started parsing "${inName}".`);
 
             const codFile = this.findFileCaseInsensitive(annoRoot, inName);
             const codStream = await Stream.fromZipObject(codFile);
             await this.fs.write(outName, parser.decrypt(codStream));
 
-            log.info(`Finished parsing "${inName}".`);
+            this.logger.info(`Finished parsing "${inName}".`);
         }));
     }
 
@@ -280,22 +250,22 @@ export default class UploadHandler {
             const inName = r[0] + ".BSH";
             const outName = r[1];
 
-            log.info(`Started parsing "${inName}".`);
+            this.logger.info(`Started parsing "${inName}".`);
             const bshFile = this.findFileCaseInsensitive(annoRoot, inName);
             const images = await parser.decodeBSH(await Stream.fromZipObject(bshFile));
             const sheets = parser.createSpriteSheets(images);
             await parser.saveSpriteSheets(this.fs, sheets, `/gfx/${outName}`);
-            log.info(`Finished parsing "${inName}".`);
+            this.logger.info(`Finished parsing "${inName}".`);
         }
     }
 
     private async parseTranslations(annoRoot: JSZip) {
-        log.info("Started parsing translations.");
+        this.logger.info("Started parsing translations.");
         const translationFile = this.findFileCaseInsensitive(annoRoot, "text.cod");
         const codParser = new CODParser();
         const translations = parseTranslations(codParser.decrypt(await Stream.fromZipObject(translationFile)));
         await this.fs.write("/translations.json", JSON.stringify(translations));
-        log.info("Finished parsing translations.");
+        this.logger.info("Finished parsing translations.");
 
         return translations.PATH[0];
     }
@@ -322,7 +292,7 @@ export default class UploadHandler {
         ];
         for (const fontName of files) {
             const inName = `TOOLGFX/${fontName}.ZEI`;
-            log.info(`Started parsing "${inName}".`);
+            this.logger.info(`Started parsing "${inName}".`);
 
             const zeiFile = this.findFileCaseInsensitive(annoRoot, inName);
             const images = await parser.decodeZEI(await Stream.fromZipObject(zeiFile));
@@ -332,15 +302,15 @@ export default class UploadHandler {
 
             await this.fs.write(`/fonts/${fontName}/font.xml`, font);
 
-            log.info(`Finished parsing "${inName}".`);
+            this.logger.info(`Finished parsing "${inName}".`);
         }
     }
 
     private async parseMusic(annoRoot: JSZip) {
         await this.fs.mkdir("/music");
 
-        if (!annoRoot.folder("MUSIC8")) {
-            console.warn("No music files found.");
+        if (!this.hasFolderCaseInsensitive(annoRoot, "MUSIC8")) {
+            this.logger.warn("No music files found.");
             return;
         }
 
@@ -350,7 +320,7 @@ export default class UploadHandler {
         const songs = await this.findFilesInZip(annoRoot, "MUSIC8", ".wav");
         for (const song of songs) {
             const name = song.path.substr(1);
-            log.info(`Converting song ${name}`);
+            this.logger.info(`Converting song ${name}`);
             const rawData = wavParser.decode(await song.file.async("uint8array"));
             const wavData = wavParser.encode(rawData);
 
@@ -365,15 +335,15 @@ export default class UploadHandler {
             //     mp3Data,
             // );
 
-            log.info(`Finished converting song ${name}`);
+            this.logger.info(`Finished converting song ${name}`);
         }
     }
 
     private async parseVideos(annoRoot: JSZip) {
         await this.fs.mkdir("/videos");
 
-        if (!annoRoot.folder("VIDEOSMK")) {
-            console.warn("No video files found.");
+        if (!this.hasFolderCaseInsensitive(annoRoot, "VIDEOSMK")) {
+            this.logger.warn("No video files found.");
             return;
         }
 
@@ -382,13 +352,13 @@ export default class UploadHandler {
         const videos = await this.findFilesInZip(annoRoot, "VIDEOSMK", ".smk");
         for (const video of videos) {
             const name = video.path.substr(1);
-            log.info(`Converting video ${name}`);
+            this.logger.info(`Converting video ${name}`);
 
             const videoData = await video.file.async("uint8array");
             const result = await smkParser.parse(videoData);
 
             await this.fs.write(`/videos/${name.replace(".smk", ".mp4")}`, result);
-            log.info(`Finished converting video ${name}`);
+            this.logger.info(`Finished converting video ${name}`);
         }
     }
 
@@ -416,7 +386,7 @@ export default class UploadHandler {
     private async copyFolderFromZip(zip: JSZip, inPath: string, outPath: string, fileExtensions: string,
                                     makeLowerCase: boolean = true) {
         inPath = `${inPath}/`;
-        console.debug(`Copying '${fileExtensions}' files from '${inPath}' to '${outPath}'.`);
+        this.logger.info(`Copying '${fileExtensions}' files from '${inPath}' to '${outPath}'.`);
 
         const files = this.findFilesInZip(zip, inPath, fileExtensions);
 
@@ -428,7 +398,7 @@ export default class UploadHandler {
             const file = fileAndPath.file;
 
             const targetPath = `${outPath}/${relativePath}`;
-            console.debug(`Copying '${relativePath}' to '${targetPath}'.`);
+            this.logger.info(`Copying '${relativePath}' to '${targetPath}'.`);
             results.push(this.fs.write(targetPath, await file.async("blob")));
         }
 
@@ -457,12 +427,17 @@ export default class UploadHandler {
     }
 
     private findFileCaseInsensitive(zip: JSZip, path: string): JSZip.JSZipObject {
-        const caseInsensitiveFileInName = new RegExp(escapeStringRegexp(path), "i");
-        const translationFile = zip.file(caseInsensitiveFileInName)[0];
-        if (translationFile === undefined) {
+        const caseInsensitiveFileName = new RegExp(escapeStringRegexp(path), "i");
+        const file = zip.file(caseInsensitiveFileName)[0];
+        if (file === undefined) {
             throw new Error(`Could not find file ${path}.`);
         }
 
-        return translationFile;
+        return file;
+    }
+
+    private hasFolderCaseInsensitive(zip: JSZip, path: string): boolean {
+        const caseInsensitiveFolderName = new RegExp(escapeStringRegexp(path), "i");
+        return zip.folder(caseInsensitiveFolderName)[0] !== undefined;
     }
 }
---
+++ src/uploadInfo.tsx
@@ -0,0 +1,162 @@
+import * as React from "react";
+import {ChangeEvent} from "react";
+import {UploadLogger} from "./upload";
+
+interface Props {
+    onUploaded: (file: File) => Promise<boolean>;
+    onReset: () => Promise<void>;
+    onSaveOrMissionUploaded: (evt: ChangeEvent<HTMLInputElement>) => Promise<void>;
+    setLogger: (logger: UploadLogger) => void;
+}
+
+interface State {
+    isUploading: boolean;
+    messages: Array<{msg: string, type: "info" | "warn" | "error"}>;
+}
+
+export default class UploadInfo extends React.Component<Props, State> implements UploadLogger {
+    constructor(props: Props) {
+        super(props);
+
+        this.state = {
+            isUploading: false,
+            messages: [],
+        };
+
+        this.props.setLogger(this);
+    }
+
+    public render() {
+        return <div style={{
+            marginLeft: "1em", marginRight: "1em", fontFamily: "Arial", maxWidth: "800px", lineHeight: 1.4,
+        }}>
+            <h1>Anno 2018</h1>
+            <p>
+                Welcome to Anno 2018, an effort to port twenty-year-old Anno 1602 to the browser.
+                This is in early development, however, you can already load your savegames and start
+                new games. Much of the actual simulation is still missing though. For copyright reasons,
+                we can't include the original Anno 1602 files and graphics. <strong>You need own your
+                own copy of Anno 1602 for Anno 2018 work.</strong>
+            </p>
+            <p>The following versions have been tested by us and confirmed to work:</p>
+            <ul>
+                <li>German "Anno 1602 Köngisedition"</li>
+                <li>English "Anno 1602 A.D." from GOG</li>
+            </ul>
+            <p>
+                Anno 2018 is tested in the latest versions of <strong>Chrome (preferred, faster)
+                and Firefox</strong> only.
+                Other browsers <em>might</em> work, but aren't supported.
+            </p>
+            <p>
+                To get started, follow these steps. You will need to "upload" your Anno files on this
+                website. Don't worry though, the files <em>never</em> leave your PC and are only
+                uploaded into your browser. Once uploaded, the files remain in your browser and
+                will be there when you re-open this website.
+            </p>
+            <ol>
+                <li>
+                    Find your Anno 1602 files and zip them. If you are using a version of Anno 1602
+                    which came with a physical CD, copy the videosmk and music8 folders into your
+                    Anno 1602 root folder before creating the zip file.
+                </li>
+                <li>
+                    Use the upload button below to upload the zip file. The file should be around
+                    50MB if it doesn't contain music and videos, and around 500MB if it does.
+                </li>
+                <li>
+                    <p>
+                        Grab a coffee. The upload takes some time. Progress is being logged below.
+                        Should anything go wrong, take a look at the error message. It might tell
+                        you what to do. If it doesn't, make sure you're using a recent version of
+                        Chrome or Firefox.
+                        If it is not working still, <a href="https://reddit.com/u/cmfcmf">
+                            drop me a note at reddit
+                        </a> or <a href="https://github.com/cmfcmf/Anno2018-js">
+                            create an issue at GitHub
+                        </a>. Make sure to include the upload progress output.
+                    </p>
+                    Upload: <input type={"file"} multiple={false} accept={".zip"} onChange={this.zipUploaded}
+                           disabled={this.state.isUploading} />
+                </li>
+            </ol>
+
+            <h2>Upload Progress <small>(newest at the top)</small></h2>
+            {this.state.messages.length === 0 && <p>Nothing yet...</p>}
+            <div style={{fontSize: "10px"}}>
+                {this.state.messages.map((message, i) => <div key={i}>
+                    <span style={{
+                        color: message.type === "info" ? "blue" : message.type === "warn" ? "orange" : "red"}}
+                    >
+                        {message.type.toUpperCase()}
+                    </span>: {message.msg}
+                </div>)}
+            </div>
+
+            <h2>Uploading custom missions and additional savegames</h2>
+            <p>
+                Use this button to upload additional missions or savegames after you
+                uploaded the main game: <input type={"file"} multiple={true} accept={".szm,.szs,.gam"}
+                                               onChange={this.props.onSaveOrMissionUploaded} />
+            </p>
+
+            <h2>Resetting files</h2>
+            <p>
+                You can use the "Reset all files" button to delete all uploaded files
+                if needed: <button onClick={this.resetFiles}>Reset all files</button>
+            </p>
+        </div>;
+    }
+
+    public readonly info = (msg: string) => {
+        this.log(msg, "info");
+    }
+
+    public readonly warn = (msg: string) => {
+        this.log(msg, "warn");
+    }
+
+    public readonly error = (msg: string) => {
+        this.log(msg, "error");
+    }
+
+    private readonly log = (msg: string, type: "info" | "warn" | "error") => {
+        this.setState((prevState: State) => {
+            return {
+                ...prevState,
+                messages: [{msg, type}, ...prevState.messages],
+            };
+        });
+    }
+
+    private zipUploaded = async (evt: ChangeEvent<HTMLInputElement>) => {
+        const files = evt.target.files;
+        if (files === null || files.length !== 1) {
+            alert("Please select the zipped Anno1602 folder to upload.");
+            return;
+        }
+        const file = files[0];
+        if (!file.name.endsWith(".zip")) {
+            alert("You need to upload a .zip file!");
+            return;
+        }
+
+        this.setState({isUploading: true});
+        const worked = await this.props.onUploaded(file);
+        this.setState({isUploading: false});
+
+        if (worked) {
+            this.info("Upload finished. Please reload the page.");
+            alert("Upload finished. Please reload the page.");
+        } else {
+            this.error("The upload failed.");
+            alert("The upload failed.");
+        }
+    }
+
+    private resetFiles = async () => {
+        await this.props.onReset();
+        alert("All files deleted. The page will now refresh.");
+        window.location.reload(true);
+    }
+}
