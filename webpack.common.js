723e7a2715e169b5f007dc45d3b3b74df5e3b785
Christian Flach <cmfcmf.flach@gmail.com>
2018-09-02 12:06:23 +0200

    Make TSLint problems errors again
    Reverts most of 439bdd53326adf7bda427a481c0673e96d5e613c

--- webpack.common.js
+++ webpack.common.js
@@ -2,83 +2,80 @@ const path = require("path");
 const webpack = require("webpack");
 const GitRevisionPlugin = require("git-revision-webpack-plugin");
 const gitRevisionPlugin = new GitRevisionPlugin({
-    // branch: true,
+  // branch: true,
 });
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
 module.exports = {
-    entry: {
-        app: [
-            "idb.filesystem.js",
-            "@babel/polyfill",
-            "./src/index.ts",
-        ],
-    },
-    // Extract webpack and vendor code into separate files
-    // https://webpack.js.org/guides/caching/#extracting-boilerplate
-    optimization: {
-        // Move webpack runtime data into separate file
-        runtimeChunk: 'single',
-        // Move all vendor code into separate file
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all'
-                }
+  entry: {
+    app: ["idb.filesystem.js", "@babel/polyfill", "./src/index.ts"]
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"]
             }
+          },
+          {
+            loader: "ts-loader",
+            options: {
+              transpileOnly: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  // Extract webpack and vendor code into separate files
+  // https://webpack.js.org/guides/caching/#extracting-boilerplate
+  optimization: {
+    // Move webpack runtime data into separate file
+    runtimeChunk: "single",
+    // Move all vendor code into separate file
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          chunks: "all",
+          name: "vendors",
+          test: /[\\/]node_modules[\\/]/
         }
-    },
-    plugins: [
-        gitRevisionPlugin,
-        new webpack.DefinePlugin({
-            '__VERSION__': JSON.stringify(gitRevisionPlugin.version()),
-            // '__COMMITHASH__': JSON.stringify(gitRevisionPlugin.commithash()),
-            // '__BRANCH__': JSON.stringify(gitRevisionPlugin.branch()),
-        }),
-        new CopyWebpackPlugin([
-            {from: "./node_modules/smk2mp4/demo/ffmpeg.js", to: 'ffmpeg.js'},
-            {from: "./node_modules/smk2mp4/demo/ffmpeg.js.mem", to: 'ffmpeg.js.mem'},
-        ]),
-        new HtmlWebpackPlugin({
-            title: "Anno 2018",
-            meta: {
-                viewport: "initial-scale=1, maximum-scale=1, user-scalable=no, minimum-scale=1, width=device-width, height=device-height",
-                "apple-mobile-web-app-capable": "yes",
-            }
-        }),
-        new ForkTsCheckerWebpackPlugin(),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: [
-                    {
-                        loader: "babel-loader",
-                        options: {
-                            presets: ['@babel/preset-env']
-                        },
-                    },
-                    {
-                        loader: "ts-loader",
-                        options: {
-                            transpileOnly: true,
-                        },
-                    },
-                ],
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"],
-    },
-    output: {
-        filename: "[name].bundle.js",
-        path: path.resolve(__dirname, "dist"),
-    },
+      }
+    }
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    gitRevisionPlugin,
+    new webpack.DefinePlugin({
+      __VERSION__: JSON.stringify(gitRevisionPlugin.version())
+      // '__COMMITHASH__': JSON.stringify(gitRevisionPlugin.commithash()),
+      // '__BRANCH__': JSON.stringify(gitRevisionPlugin.branch()),
+    }),
+    new CopyWebpackPlugin([
+      { from: "./node_modules/smk2mp4/demo/ffmpeg.js", to: "ffmpeg.js" },
+      { from: "./node_modules/smk2mp4/demo/ffmpeg.js.mem", to: "ffmpeg.js.mem" }
+    ]),
+    new HtmlWebpackPlugin({
+      meta: {
+        "apple-mobile-web-app-capable": "yes",
+        viewport:
+          "initial-scale=1, maximum-scale=1, user-scalable=no, minimum-scale=1, width=device-width, height=device-height"
+      },
+      title: "Anno 2018"
+    }),
+    new ForkTsCheckerWebpackPlugin()
+  ],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  }
 };
